a6036b6a91d4f64c8b50150c43df1dacf84e96ed
Nivang Goyal <nivang231@gmail.com>
2025-12-05 15:51:35 +0530

    Updated-2

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,11 +1,28 @@
 import { useParams } from "react-router-dom";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const ProductDetails = () => {
-  const { id } = useParams();
+  const params = useParams();
+  const [apiData, setApiData] = useState([]);
+   
+
+    useEffect(()=>{
+         const fetchData = async ()=>{
+        const {data} = await axios(`https://fakestoreapi.com/products/${params.id}`);
+        setApiData(data);
+        console.log(data)
+    };
+        fetchData();
+    },[])
 
   return (
-    <div className="p-5">
-      <h1 className="text-2xl font-bold">Product ID: {id}</h1>
+    <div className="p-5 border-2 w-[400px]">  
+      <h1 className="text-2xl font-bold">{params.id}</h1>
+      <img src={apiData.image} alt="" />
+      <p className="bg-green-400 w-fit">⭐{apiData?.rating?.rate}</p>
+      <p className="bg-violet-500 w-fit">${apiData.price}</p>
     </div>
   );
 };
